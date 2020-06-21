63765fe57bb93ec8a2d0379306b205b57b05ebe6
Surya Makthal <smaktha@ncsu.edu>
2020-06-21 16:14:40 -0400

    only require name of pokemon and game version as arguments

--- find.py
+++ find.py
@@ -2,20 +2,12 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
-try:
-    num = sys.argv[1]
-    if int(num) > 493:
-        raise Exception()
-    version = sys.argv[2]
+from lookup import games, all_mons
 
-    poke = ''
-    method = ''
-
-    url = "https://www.serebii.net/pokedex-dp/{}.shtml".format(num)
+def find(mon, gen, dex, version):
+    url = "https://www.serebii.net/{}/{}{}".format(dex, str(all_mons[mon]).zfill(3) if gen < 8 else mon, '.shtml' if gen < 8 else '')
     r = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(r.text, 'html.parser')
-    # print(str(soup.find('title').next_element))
-    poke = str(soup.find('title').next_element).split(num)[1].strip()
     tr_filter = soup.find_all("tr")
     for tr in tr_filter:
         version_tag = tr.find_all("td", {"class" : "{}".format(version)})
@@ -24,7 +16,13 @@ try:
                 location_tag = td.parent.find("td", {"class", "fooinfo"})
                 if location_tag:
                     method = str(location_tag.contents[0]).strip()
-                    break
+                    return method
+
+try:
+    poke = sys.argv[1]
+    version = sys.argv[2]
+
+    method = find(poke.lower(), games[version][0], games[version][1], games[version][2])
 
     print()
     print(poke.upper())
